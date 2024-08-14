95b8e6de96461b01b1fe6a9c929de443437847a7
carlos bernal <bernal199152@yahoo.com>
2024-08-14 12:40:36 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer; 
+const leastFavoriteCustomer = 'peter'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    console.log(customerName);
+
+    
+}
+
+function setBestCustomer (){
+   bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer (customer) {
+    leastFavoriteCustomer = 'peter'
+    console.log(leastFavoriteCustomer);
+}
+
+changeLeastFavoriteCustomer ();
+
+
+
